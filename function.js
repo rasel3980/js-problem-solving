81260b9db4ddd943cd0ee21d9b05f00e4b683d5c
rasel3980 <rkrasel3980@gmail.com>
2025-12-11 12:48:38 +0600

    Even or Odd number Operation

--- function.js
+++ function.js
@@ -8,3 +8,18 @@ function multiply(num,num2,num3,num4){
 
 const resultMultiply = multiply(4,6,8,9);
 console.log(resultMultiply);
+
+// Task-2
+// Take a number if the number is odd multiply it by 2 and return the result. If the number is even divide it by two and return the result.
+
+function evenOrOddOperation(number){
+    if(number % 2 === 1){
+        return number*2;
+    }
+    else{
+        return number/2
+    }
+}
+
+const resultEvenOrOddOperation = evenOrOddOperation(9)
+console.log(resultEvenOrOddOperation);
